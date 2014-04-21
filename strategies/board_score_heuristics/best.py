9273d6e1cdcc2e154ac1ebf3656089063164dd65
Eran Shirazi <EranShirazi@gmail.com>
2014-04-21 19:20:58 +0300

    bugfix

--- strategies/board_score_heuristics/best.py
+++ strategies/board_score_heuristics/best.py
@@ -2,5 +2,5 @@ from strategies.board_score_heuristics.claustrophobic import claustrophobic_heur
 from strategies.board_score_heuristics.side_sticky import side_sticky_heuristic
 from strategies.board_score_heuristics.sum_square import sum_square_heuristic
 
-ace_heuristic = lambda board: sum_square_heuristic(board) * side_sticky_heuristic(board)
+ace_heuristic = lambda board: sum_square_heuristic(board) + side_sticky_heuristic(board)
 king_heuristic = lambda board: sum_square_heuristic(board) * claustrophobic_heuristic(board)
